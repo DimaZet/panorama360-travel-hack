dfd468954244e06252d6f84435b3202b6f5aae83
denis.oskin <denis.oskin@glowbyteconsulting.com>
2020-02-08 17:13:09 +0300

    Add background setter

--- src/main/java/dino/party/imageapi/service/BackgroundCutter.java
+++ src/main/java/dino/party/imageapi/service/BackgroundCutter.java
@@ -12,18 +12,22 @@ import static marvinplugins.MarvinPluginCollection.alphaBoundary;
 
 public class BackgroundCutter {
 
-    public byte[] ChromaToTransparency(byte[] bImage, String format) throws IOException {
+    public byte[] ChromaToBackground(byte[] bImage, byte[] bBackground, String format) throws IOException {
         MarvinImage image = new MarvinImage(ImageIO.read(new ByteArrayInputStream(bImage)));
         MarvinImage imageOut = new MarvinImage(image.getWidth(), image.getHeight());
         // Convert green to transparency
         greenToTransparency(image, imageOut);
         // Reduce extra green pixels
         reduceGreen(imageOut);
+        // Set background
+        MarvinImage background = new MarvinImage(ImageIO.read(new ByteArrayInputStream(bBackground)));
+        setBackground(imageOut, background);
         // Smoothing angles
         alphaBoundary(imageOut, 7);
+        // Convert image to bytes
         imageOut.update();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(imageOut.getBufferedImage(), format, baos);;
+        ImageIO.write(imageOut.getBufferedImage(), format, baos);
         return  baos.toByteArray();
     }
 
@@ -55,13 +59,27 @@ public class BackgroundCutter {
                 int color = image.getIntColor(x, y);
                 double[] hsv = MarvinColorModelConverter.rgbToHsv(new int[]{color});
 
-                if(hsv[0] >= 60 && hsv[0] <= 160 && hsv[1] >= 0.15 && hsv[2] >= 0.15){
+                if(hsv[0] >= 50 && hsv[0] <= 160 && hsv[1] >= 0.15 && hsv[2] >= 0.15){
                     if((r*b) !=0 && (g*g) / (r*b) > 1.5){
-                        image.setIntColor(x, y, 255, (int)(r*1.4), (int)g, (int)(b*1.4));
+                        image.setIntColor(x, y, 255, (int)(r*1.4), g, (int)(b*1.4));
                     } else{
                         image.setIntColor(x, y, 255, (int)(r*1.2), g, (int)(b*1.2));
                     }
                 }
+            }
+        }
+    }
+
+    private void setBackground(MarvinImage image, MarvinImage background)
+    {
+        for(int y=0; y<image.getHeight(); y++){
+            for(int x=0; x<image.getWidth(); x++){
+                int alpha = image.getAlphaComponent(x,y);
+                int r = background.getIntComponent0(x, y);
+                int g = background.getIntComponent1(x, y);
+                int b = background.getIntComponent2(x, y);
+                if(alpha == 0)
+                    image.setIntColor(x, y, 255, r, g, b);
             }
         }
     }
