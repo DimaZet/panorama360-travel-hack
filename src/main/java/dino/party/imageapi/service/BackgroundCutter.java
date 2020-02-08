70875efdabc13bacb604cc697d824afc99e7aa26
denis.oskin <denis.oskin@glowbyteconsulting.com>
2020-02-08 13:08:26 +0300

    Restructure i/o to bytes Improving quality of output image

--- src/main/java/dino/party/imageapi/service/BackgroundCutter.java
+++ src/main/java/dino/party/imageapi/service/BackgroundCutter.java
@@ -2,24 +2,29 @@ package dino.party.imageapi.service;
 
 import marvin.color.MarvinColorModelConverter;
 import marvin.image.MarvinImage;
-import marvin.io.MarvinImageIO;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import static marvinplugins.MarvinPluginCollection.alphaBoundary;
 
 public class BackgroundCutter {
 
-    public void ChromaToTransparency(String filepath, String format){
-        //MarvinImage image = new MarvinImage().setBufferedImage();
-        MarvinImage image = MarvinImageIO.loadImage(filepath + "." + format);
+    public byte[] ChromaToTransparency(byte[] bImage, String format) throws IOException {
+        MarvinImage image = new MarvinImage(ImageIO.read(new ByteArrayInputStream(bImage)));
         MarvinImage imageOut = new MarvinImage(image.getWidth(), image.getHeight());
-        // 1. Convert green to transparency
+        // Convert green to transparency
         greenToTransparency(image, imageOut);
-        // 2. Reduce remaining green pixels
+        // Reduce extra green pixels
         reduceGreen(imageOut);
-        MarvinImageIO.saveImage(imageOut, filepath + "_out1.png");
-        // 3.
-        alphaBoundary(imageOut, 6);
-        MarvinImageIO.saveImage(imageOut, filepath + "_out2.png");
+        // Smoothing angles
+        alphaBoundary(imageOut, 7);
+        imageOut.update();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(imageOut.getBufferedImage(), format, baos);;
+        return  baos.toByteArray();
     }
 
     private void greenToTransparency(MarvinImage imageIn, MarvinImage imageOut){
@@ -30,7 +35,7 @@ public class BackgroundCutter {
 
                 double[] hsv = MarvinColorModelConverter.rgbToHsv(new int[]{color});
 
-                if(hsv[0] >= 50 && hsv[0] <= 170 && hsv[1] >= 0.4 && hsv[2] >= 0.3){
+                if(hsv[0] >= 50 && hsv[0] <= 160 && hsv[1] >= 0.4 && hsv[2] >= 0.3){
                     imageOut.setIntColor(x, y, 0, 127, 127, 127);
                 }
                 else{
@@ -50,7 +55,7 @@ public class BackgroundCutter {
                 int color = image.getIntColor(x, y);
                 double[] hsv = MarvinColorModelConverter.rgbToHsv(new int[]{color});
 
-                if(hsv[0] >= 60 && hsv[0] <= 130 && hsv[1] >= 0.15 && hsv[2] >= 0.15){
+                if(hsv[0] >= 60 && hsv[0] <= 160 && hsv[1] >= 0.15 && hsv[2] >= 0.15){
                     if((r*b) !=0 && (g*g) / (r*b) > 1.5){
                         image.setIntColor(x, y, 255, (int)(r*1.4), (int)g, (int)(b*1.4));
                     } else{
